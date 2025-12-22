26e3786ab2f4a9705d7b475207562d853edecc24
achyut479 <achyutshashanksingh1@gmail.com>
2025-12-22 19:50:54 +0530

    Add files via upload

---
+++ string_opration.py
@@ -0,0 +1,15 @@
+first_name = "Achyut"
+last_name = "Singh"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("first_name:", first_name)
+print("last_name:", last_name)
+print("full_name:", full_name)
+print("string Multiplied 5 times gives this result:", example)
+
+word = 'coding'
+print("Length of String is:", len(word))
+print("First letter of word is:", word[0])
+print("Last letter of word is:", word[5])
+print("String Sliced :" word[0:3])
