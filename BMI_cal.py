84c4097d8bf2ed16f1f82396381d447cbe1c1490
achyut479 <achyutshashanksingh1@gmail.com>
2025-12-23 19:49:08 +0530

    Implement BMI calculation and underweight message
    Added BMI calculation and underweight check.

---
+++ BMI_cal.py
@@ -0,0 +1,13 @@
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is", BMI)
+
+if BMI <= 18.4:
+  print("You are underweight: ")
+
+
+    
+  
