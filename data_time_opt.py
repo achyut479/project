10c3a37feedb5549ec68ea96f4c37c4eb08aebc7
achyut479 <achyutshashanksingh1@gmail.com>
2025-12-23 19:57:25 +0530

    Initialize current time and print calendar
    Fix import typo and adjust print statements.

---
+++ data_time_opt.py
@@ -0,0 +1,12 @@
+inport datatime
+
+# using now() to get current time
+current_time = datatime.datatime.now()
+
+# printing value of now.
+print("time now at greenwich meridian is : ", end = "")
+print(current_time)
+
+# print calendar of year 2023
+import calendar
+print("\n", calendar.calendar(2021)
