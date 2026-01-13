4266a297a03b36edef33c5f591a23d5147eb0211
achyut479 <achyutshashanksingh1@gmail.com>
2026-01-13 19:35:08 +0530

    Define parrot class with attributes and instances
    Fix class definition and instantiation errors in parrot_bird.py.

---
+++ parrot_bird.py
@@ -0,0 +1,14 @@
+class parrot:
+
+    # class attribute
+    species = "bird"
+
+   # instance attribute
+  def_init_(self,age,name):
+      self.name = name
+      self.age = age
+
+# instantiate the parrot  class
+blu = parrot("blu", 10):
+woo = parrot("woo", 15):
+ print(blu+woo)
