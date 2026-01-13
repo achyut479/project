afef9d98001c9c76727a3b49fd780004dca058e4
achyut479 <achyutshashanksingh1@gmail.com>
2026-01-13 19:25:04 +0530

    Create student class with methods for introduction and details
    Fix object instantiation and method calls.

---
+++ class_student_2.py
@@ -0,0 +1,14 @@
+class student:
+    grade = 10
+    name = "achyut"
+
+    def introduction(self):
+        print("hi i am a student")
+
+    def details(self):
+        print("my name is", self.name)
+        print("i am study in grade", self.grade)
+
+  ob =student()
+  ob =introduction()
+  ob =details()
